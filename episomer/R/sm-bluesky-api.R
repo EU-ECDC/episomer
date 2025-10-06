5052ce9526376314258f833bf98d999f843fad02
Yohann Mansiaux <y.mansiaux@epiconcept.fr>
2025-10-06 14:27:42 +0200

    feat: add specific tags to catch functions declarations

--- episomer/R/sm-bluesky-api.R
+++ episomer/R/sm-bluesky-api.R
@@ -3,9 +3,11 @@
 #' @return Token
 #' @export
 #' @rdname session
+# @function_def_start (do not delete)
 sm_api_get_token_bluesky <- function() {
-  if (is_secret_set("bsky_session")) {
-    session <- jsonlite::fromJSON(get_secret("bsky_session"))
+# @function_def_end (do not delete)
+  if (is_secret_set("bluesky_session")) {
+    session <- jsonlite::fromJSON(get_secret("bluesky_session"))
     access_jwt <- bluesky_check_token_validity(session$access_jwt)
     if (session$created < strptime(Sys.time() - 3600, "%Y-%m-%d %H:%M:%S")) {
       session <- bluesky_create_session()
@@ -16,7 +18,10 @@ sm_api_get_token_bluesky <- function() {
   return(bluesky_create_session()$access_jwt)
 }
 
+
+# @function_def_start (do not delete)
 sm_api_translate_query_bluesky <- function(parts, excluded) {
+# @function_def_end (do not delete)
   queries = list()
   comb = do.call(expand.grid, parts)
   if (nrow(comb) > 0) {
@@ -37,12 +42,13 @@ sm_api_translate_query_bluesky <- function(parts, excluded) {
 #' @param max_retries Maximum number of retries
 #' @param errors_for_retries Errors for retries
 #' @param verbose Whether to print progress messages
-#' @details You can find more information about the Bluesky API here: \url{https://docs.bsky.app/docs/api/app-bsky-feed-search-posts}
+#' @details You can find more information about the bluesky API here: \url{https://docs.bsky.app/docs/api/app-bsky-feed-search-posts}
 #' Additionnaly more information about the "post" object is available here: \url{https://atproto.blue/en/latest/atproto/atproto_client.models.app.bsky.feed.defs.html#atproto_client.models.app.bsky.feed.defs.PostView}
 #' @return List with posts and next cursor for resumption
 #' @export
 #' @importFrom httr2 request req_url_query req_headers req_perform resp_body_json is_online resp_status last_response req_retry
 #' @importFrom lubridate as_datetime
+# @function_def_start (do not delete)
 sm_api_search_bluesky <- function(
   query,
   token,
@@ -51,6 +57,7 @@ sm_api_search_bluesky <- function(
   errors_for_retries = c(420, 500, 503),
   verbose = TRUE
 ) {
+# @function_def_end (do not delete)
   search_url = "https://bsky.social/xrpc/app.bsky.feed.searchPosts"
   sort <- "latest"
   number_of_posts_per_request <- 100
@@ -93,7 +100,9 @@ sm_api_search_bluesky <- function(
 }
 
 #' @noRd
+# @function_def_start (do not delete)
 bluesky_rate_limited_check <- function(resp) {
+# @function_def_end (do not delete)
   if (httr2::resp_status(resp) == 429) {
     identical(httr2::resp_header(resp, "RateLimit-Remaining"), "0")
   } else if (httr2::resp_status(resp) == 503) {
@@ -104,7 +113,9 @@ bluesky_rate_limited_check <- function(resp) {
 }
 
 #' @noRd
+# @function_def_start (do not delete)
 bluesky_rerun_after_rate_limit <- function(resp) {
+# @function_def_end (do not delete)
   if (httr2::resp_status(resp) == 429) {
     time <- as.numeric(resp_header(resp, "RateLimit-Reset"))
     time - unclass(Sys.time())
@@ -114,18 +125,24 @@ bluesky_rerun_after_rate_limit <- function(resp) {
 }
 
 #' @noRd
+# @function_def_start (do not delete)
 bluesky_format_date <- function(datetime) {
+# @function_def_end (do not delete)
   timezone = "UTC"
   # Full ISO 8601 format with time
   format(datetime, "%Y-%m-%dT%H:%M:%OS6Z", tz = "UTC")
 }
 
 #' @noRd
+# @function_def_start (do not delete)
 bluesky_parse_date <- function(date_input) {
+# @function_def_end (do not delete)
   lubridate::as_datetime(unlist(date_input))
 }
 #' @noRd
+# @function_def_start (do not delete)
 bluesky_parse_response <- function(response) {
+# @function_def_end (do not delete)
   first <- function(x) unlist(x[min(1, length(x))])
   res = list(
     network = "bluesky",
@@ -183,7 +200,9 @@ bluesky_parse_response <- function(response) {
   res
 }
 
+# @function_def_start (do not delete)
 bluesky_parse_quoted <- function(post) {
+# @function_def_end (do not delete)
   # utilitary functions
   first <- function(x) {
     x <- x[!sapply(x, is.null)]
@@ -352,7 +371,9 @@ bluesky_parse_quoted <- function(post) {
   }
 }
 
+# @function_def_start (do not delete)
 bluesky_parse_features <- function(post) {
+# @function_def_end (do not delete)
   # extracting features data from post
   ret <- list(tags = list(), urls = list(), categories = list())
   if ("record" %in% names(post) && "facets" %in% names(post$record)) {
@@ -394,30 +415,32 @@ bluesky_parse_features <- function(post) {
 
 #' Get bearer token
 #'
-#' @param handle Bluesky handle
-#' @param password Bluesky password
-#' @param login_url Bluesky login URL
+#' @param handle bluesky handle
+#' @param password bluesky password
+#' @param login_url bluesky login URL
 #'
 #' @return List with access_jwt and did
 #' @export
 #' @importFrom httr2 request req_body_json req_perform resp_check_status resp_body_json is_online req_headers
-#' @details You can find more information about the Bluesky API here: \url{https://docs.bsky.app/docs/api/com-atproto-server-create-session}
+#' @details You can find more information about the bluesky API here: \url{https://docs.bsky.app/docs/api/com-atproto-server-create-session}
 #' @rdname session
 #' @examples
 #' \dontrun{
 #' bluesky_create_session()
 #' }
 #'
+# @function_def_start (do not delete)
 bluesky_create_session <- function(handle = NULL, password = NULL) {
-  if (is.null(handle) && is_secret_set("bsky_user")) {
-    handle = get_secret("bsky_user")
+# @function_def_end (do not delete)
+  if (is.null(handle) && is_secret_set("bluesky_user")) {
+    handle = get_secret("bluesky_user")
   } else if (is.null(handle) && Sys.getenv("bluesky_id") != "")
     handle = Sys.getenv("bluesky_id") else {
     stop("You have to provide a user_name to collect Bluesky posts")
   }
 
-  if (is.null(password) && is_secret_set("bsky_password")) {
-    password = get_secret("bsky_password")
+  if (is.null(password) && is_secret_set("bluesky_password")) {
+    password = get_secret("bluesky_password")
   } else if (is.null(handle) && Sys.getenv("bluesky_pwd") != "")
     password = Sys.getenv("bluesky_pwd") else {
     stop("You have to provide a password to collect Bluesky posts")
@@ -443,7 +466,7 @@ bluesky_create_session <- function(handle = NULL, password = NULL) {
     created = strptime(Sys.time(), "%Y-%m-%d %H:%M:%S")
   )
   json_session = jsonlite::toJSON(session)
-  set_secret("bsky_session", json_session)
+  set_secret("bluesky_session", json_session)
   return(jsonlite::fromJSON(json_session))
 }
 
@@ -451,10 +474,12 @@ bluesky_create_session <- function(handle = NULL, password = NULL) {
 #' @noRd
 #' @rdname session
 #' @importFrom httr2 request req_url_query req_headers req_error req_perform resp_status
+# @function_def_start (do not delete)
 bluesky_check_token_validity <- function(
   access_jwt,
   search_url = "https://bsky.social/xrpc/app.bsky.feed.searchPosts"
 ) {
+# @function_def_end (do not delete)
   simple_request <- httr2::request(search_url) |>
     httr2::req_url_query(q = "covid19", limit = 1, sort = "latest") |>
     httr2::req_headers(Authorization = paste("Bearer", access_jwt)) |>
@@ -465,4 +490,35 @@ bluesky_check_token_validity <- function(
     access_jwt <- bluesky_create_session()$access_jwt
   }
   return(access_jwt)
+}
+
+
+#' @title Save bluesky credentials (login and password) and store them securely
+#' @description Update configuration object and underlying storage with given bluesky username and password. The password is encrypted.
+#' @param shiny_input_list List of shiny input values containing the bluesky specific credentials
+#' @details Update authentication tokens in configuration object
+#' @examples
+#' if(FALSE){
+#'  #Setting the configuration values
+#'    set_bluesky_auth(
+#'      shiny_input_list = list(bluesky_user = "your user here", bluesky_password = "your password"),
+#'    )
+#' }
+#' @seealso
+#' \code{\link{save_config}}
+#' @rdname set_auth
+#' @export
+# @function_def_start (do not delete)
+set_bluesky_auth <- function(shiny_input_list) {
+# @function_def_end (do not delete)
+  if(!"bluesky_user" %in% names(shiny_input_list)) {
+    stop("bluesky_user is not in the shiny_input_list")
+  }
+  if(!"bluesky_password" %in% names(shiny_input_list)) {
+    stop("bluesky_password is not in the shiny_input_list")
+  }
+  conf$auth$bluesky_user <- shiny_input_list$bluesky_user
+  conf$auth$bluesky_password <- shiny_input_list$bluesky_password
+  set_secret("bluesky_user", shiny_input_list$bluesky_user)
+  set_secret("bluesky_password", shiny_input_list$bluesky_password)
 }
